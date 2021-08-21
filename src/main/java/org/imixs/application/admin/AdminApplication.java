bdffc3b4d1e164d6e4781af09dcf3cd7d5588355
Ralph Soika <ralph.soika@imixs.com>
2021-08-21 16:29:29 +0200

    draft

---
+++ src/main/java/org/imixs/application/admin/AdminApplication.java
@@ -0,0 +1,13 @@
+package org.imixs.application.admin;
+
+import jakarta.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+	
+
+@ApplicationPath("/api")
+public class AdminApplication extends Application {
+ 
+	
+	
+	
+}
