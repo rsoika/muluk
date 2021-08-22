28438004c7cda93d85df0dbc6eaf850b4bef1ba1
Ralph Soika <ralph.soika@imixs.com>
2021-08-22 11:15:19 +0200

    update

--- src/main/java/org/imixs/muluk/AdminApplication.java
+++ src/main/java/org/imixs/muluk/AdminApplication.java
@@ -1,13 +1,9 @@
-package org.imixs.application.admin;
+package org.imixs.muluk;
 
-import jakarta.ws.rs.ApplicationPath;
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-	
 
 @ApplicationPath("/api")
 public class AdminApplication extends Application {
- 
-	
-	
-	
+
 }
